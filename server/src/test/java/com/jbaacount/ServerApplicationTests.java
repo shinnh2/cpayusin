22a5fd83f77fadc50189a8dba4c1265912c28839
ge0nmo <ands0927@naver.com>
2023-07-01 15:25:36 +0900

    backend commit

---
+++ server/src/test/java/com/jbaacount/ServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.jbaacount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
