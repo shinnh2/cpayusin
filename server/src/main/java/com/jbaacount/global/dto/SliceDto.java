29a55f3da944512a3df8d50b1ed68ad18db54079
ge0nmo <ands0927@naver.com>
2023-07-14 16:50:25 +0900

    feat/Create SliceDto

--- server/src/main/java/com/jbaacount/global/dto/SliceDto.java
+++ server/src/main/java/com/jbaacount/global/dto/SliceDto.java
@@ -1,6 +1,5 @@
 package com.jbaacount.global.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Slice;
@@ -9,13 +8,13 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor
-public class sliceDto<T>
+public class SliceDto<T>
 {
     private List<T> data;
 
     private SliceInfo sliceInfo;
 
-    public sliceDto(List<T> data, Slice slice)
+    public SliceDto(List<T> data, Slice slice)
     {
         this.data = data;
         this.sliceInfo = new SliceInfo(slice.getNumber(), slice.getSize(), slice.getNumberOfElements(), slice.hasNext(), slice.hasPrevious());
