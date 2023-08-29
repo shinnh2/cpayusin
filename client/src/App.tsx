af261952c0b9d9b1f541ad9f41bf1f2670bb420a
shinnh2 <nahyeon.shin9@gmail.com>
2023-08-29 21:34:13 +0900

    [feat/fe-baseComponents] Input 컴포넌트 생성 후 스토리북 등록

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Button from "./components/Button";
+import Input from "./components/Input";
 
 function App() {
 	return (
@@ -8,6 +9,12 @@ function App() {
 			<header>JBaccount HOME</header>
 			<section>
 				<Button buttonType="no_em" buttonSize="big" buttonLabel="테스트" />
+				<Input
+					InputLabel="테스트"
+					isLabel={true}
+					errorMsg="이것은 오류일 때 나타납니다"
+					inputAttr={{ type: "text", placeholder: "입력하세요" }}
+				/>
 			</section>
 		</div>
 	);
