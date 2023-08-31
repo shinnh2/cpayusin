b7393aaabadb67fc98ae1b42bb93f447de7e6d30
shinnh2 <nahyeon.shin9@gmail.com>
2023-08-31 21:11:54 +0900

    [feat/fe-header] Header 컴포넌트 스토리북에 등록

--- client/src/stories/Header.stories.ts
+++ client/src/stories/Header.stories.ts
@@ -1,27 +1,47 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta } from "@storybook/react";
 
-import { Header } from './Header';
+import Header from "./../components/Header";
+import { HeaderProps } from "./../components/Header";
 
 const meta = {
-  title: 'Example/Header',
-  component: Header,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
-  parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
-  },
-} satisfies Meta<typeof Header>;
+	title: "Component/Header",
+	component: Header,
+	// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+	tags: ["autodocs"],
+	parameters: {
+		// More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+		layout: "fullscreen",
+	},
+	argTypes: {
+		isLogin: {
+			control: {
+				type: "boolean", //로그인 여부를 나타냅니다.
+			},
+		},
+	},
+} satisfies Meta<HeaderProps>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+// type Story = StoryObj<typeof meta>;
 
-export const LoggedIn: Story = {
-  args: {
-    user: {
-      name: 'Jane Doe',
-    },
-  },
+export const loginHeader: Meta<HeaderProps> = {
+	args: {
+		isLogin: true,
+	},
 };
 
-export const LoggedOut: Story = {};
+export const logoutHeader: Meta<HeaderProps> = {
+	args: {
+		isLogin: false,
+	},
+};
+
+// export const LoggedIn: Story = {
+// 	args: {
+// 		user: {
+// 			name: "Jane Doe",
+// 		},
+// 	},
+// };
+
+// export const LoggedOut: Story = {};
