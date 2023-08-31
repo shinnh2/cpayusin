e9c04d674fcd73c90611839fb7743182115e96ab
shinnh2 <35826626+shinnh2@users.noreply.github.com>
2023-08-31 16:58:48 +0900

    [feat/fe-buttonInput] 공통 버튼, Input 컴포넌트 제작 (#32)
    * [feat/fe-baseComponents] Button 컴포넌트 생성 후 storybook 등록
    
    * [feat/fe-baseComponents] Input 컴포넌트 생성 후 스토리북 등록

---
+++ client/src/components/Input.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "../App.css";
+
+export interface InputProps {
+	InputLabel: string;
+	isLabel: boolean; //label의 유무를 나타냅니다.
+	inputAttr: {
+		type: string;
+		placeholder: string;
+		id?: string;
+	};
+	errorMsg?: string;
+}
+
+let count = 0;
+
+const Input = ({ InputLabel, isLabel, errorMsg, inputAttr }: InputProps) => {
+	const id = `jb-input-${count + 1}` ?? inputAttr.id;
+	return (
+		<div>
+			{isLabel ? <label htmlFor={id}>{InputLabel}</label> : null}
+			<input
+				id={id}
+				type={inputAttr.type}
+				placeholder={inputAttr.placeholder}
+			/>
+			{errorMsg ? <p className="error_msg">{errorMsg}</p> : null}
+		</div>
+	);
+};
+export default Input;
+
+///////////////
+
+// import colors from "_tosslib/constants/colors";
+// import useId from "_tosslib/hooks/useId";
+// import {
+// 	Children,
+// 	cloneElement,
+// 	ForwardedRef,
+// 	forwardRef,
+// 	HTMLAttributes,
+// 	InputHTMLAttributes,
+// 	ReactElement,
+// 	ReactNode,
+// } from "react";
+
+// interface InputProps extends HTMLAttributes<HTMLDivElement> {
+// 	label?: ReactNode;
+// 	children: ReactElement;
+// 	bottomText?: string;
+// }
+
+// export function Input({ label, children, bottomText, ...props }: InputProps) {
+// 	const child = Children.only(children); //하위 요소를 하나만 받는다
+// 	const generatedId = useId("input"); //커스텀 훅을 사용해 id를 생성
+// 	const id = child.props.id ?? generatedId; //child의 id가 없을 경우 기본값으로 훅으로 생성된 id로 설정한다.
+// 	const isError: boolean = child.props.error ?? false; //왜 있지.. 에러가 있다면 표시, 기본값은 false
+
+// 	return (
+// 		<div {...props}>
+// 			<label htmlFor={id}>{label}</label>
+// 			{cloneElement(child, {
+// 				id,
+// 				...child.props,
+// 			})}
+// 			{bottomText != null ? <p>{bottomText}</p> : null}
+// 		</div>
+// 	);
+// }
+
+// interface TextFieldProps
+// 	extends Omit<InputHTMLAttributes<HTMLInputElement>, "size"> {
+// 	error?: boolean;
+// }
+
+// Input.TextField = forwardRef(
+// 	(
+// 		{ error, ...props }: TextFieldProps,
+// 		ref: ForwardedRef<HTMLInputElement>
+// 	) => {
+// 		return <input ref={ref} {...props} />;
+// 	}
+// );
