dcf5a0aa66961f034293c71fd2954c0ce61d747a
shinnh2 <35826626+shinnh2@users.noreply.github.com>
2023-09-14 06:29:56 +0900

    [feat/fe-navFold] 네비게이션 접힘 및 펼치기 기능 추가 (#35)
    * [feat/fe-navFold] 네비게이션 접힘, 펼침 기능 추가
    
    * [feat/fe-navFold] 네비게이션 위치 조정 및 메인 영역 추가
    
    * [feat/fe-navFold] 네비게이션 및 레이아웃 관련 CSS 추가

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,25 +1,34 @@
 import boardData from "./../data/boardData.json";
 import { ReactComponent as IconHome } from "./../assets/icon_home.svg";
 import iconArrowDown from "./../assets/icon_arrow_down.svg";
+import { MouseEvent } from "react";
 
 const createMenuNode = (data: any[]) => {
 	return data.map((el: any, idx: number) => (
 		<li key={idx} className="nav_menu_item">
 			{el.name ? (
-				<a href="./">
+				<a href="" onClick={clickHandler}>
 					{el.name}
-					<button className="toggle_updown up">
-						<img src={iconArrowDown} alt="메뉴 펼치기 접기 토글 아이콘" />
-					</button>
+					{el.categories && el.categories.length !== 0 ? (
+						<i className="toggle_updown">
+							<img src={iconArrowDown} alt="메뉴 펼치기 접기 토글 아이콘" />
+						</i>
+					) : null}
 				</a>
 			) : (
-				<a href="./">{el.categoryName}</a>
+				<a href="">{el.categoryName}</a>
 			)}
 			{el.categories && el.categories.length !== 0 ? (
 				<ul className="nav_menu depth2">{createMenuNode(el.categories)}</ul>
 			) : null}
 		</li>
 	));
+};
+
+const clickHandler = (event: React.MouseEvent<HTMLAnchorElement>): void => {
+	event.preventDefault();
+	console.log(event.currentTarget.classList);
+	event.currentTarget.classList.toggle("unfold");
 };
 
 const Nav = () => {
