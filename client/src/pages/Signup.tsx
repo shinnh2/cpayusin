9efbdbbf76364d6b29e1c708b0af355e2af5e570
shinnh2 <nahyeon.shin9@gmail.com>
2023-09-15 03:17:30 +0900

    [feat/fe-signUp] 회원가입 페이지 컴포넌트 생성

---
+++ client/src/pages/Signup.tsx
@@ -0,0 +1,59 @@
+import Input from "./../components/Input";
+import Button from "./../components/Button";
+
+const Signup = () => {
+	return (
+		<div className="input_box sign_box col_4">
+			<h3 className="title_h3">회원가입</h3>
+			<div className="validate_email_wrap">
+				<Input
+					InputLabel="이메일"
+					isLabel={true}
+					errorMsg="올바른 이메일을 입력해 주세요."
+					inputAttr={{ type: "text", placeholder: "이메일을 입력하세요" }}
+				/>
+				<Button
+					buttonType="primary"
+					buttonSize="big"
+					buttonLabel="이메일 인증"
+				/>
+				<p className="validate_email_msg">이메일 인증이 완료되었습니다.</p>
+				<p className="validate_email_msg error">이미 가입한 이메일입니다.</p>
+			</div>
+			<div className="content">
+				<Input
+					InputLabel="비밀번호 입력"
+					isLabel={true}
+					errorMsg="비밀번호는 8~20자의 영문, 숫자가 포함되어야 합니다. "
+					inputAttr={{ type: "password", placeholder: "비밀번호를 입력하세요" }}
+				/>
+				<Input
+					InputLabel="비밀번호 확인"
+					isLabel={true}
+					errorMsg="비밀번호가 일치하지 않습니다."
+					inputAttr={{
+						type: "password",
+						placeholder: "비밀번호를 한 번 더 입력하세요",
+					}}
+				/>
+				<Input
+					InputLabel="닉네임"
+					isLabel={true}
+					errorMsg="공백 포함 2~10자 사이의 영문, 한글, 숫자만 가능합니다."
+					inputAttr={{
+						type: "text",
+						placeholder: "닉네임을 입력하세요",
+					}}
+				>
+					<Button
+						buttonType="another"
+						buttonSize="big"
+						buttonLabel="중복 확인"
+					/>
+				</Input>
+				<Button buttonType="primary" buttonSize="big" buttonLabel="로그인" />
+			</div>
+		</div>
+	);
+};
+export default Signup;
