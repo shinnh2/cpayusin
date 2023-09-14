44ba63fb55bfd2d5e0dc1ea9f3daadc4d4eaa572
shinnh2 <nahyeon.shin9@gmail.com>
2023-09-14 19:39:42 +0900

    [feat/fe-login] 불필요한 컴포넌트 제거 및 정리

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import "./App.css";
-import Button from "./components/Button";
-import Input from "./components/Input";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -19,15 +17,6 @@ function App() {
 							<Route path="/login" Component={Login} />
 						</Routes>
 					</Router>
-					{/* <section>
-						<Button buttonType="no_em" buttonSize="big" buttonLabel="테스트" />
-						<Input
-							InputLabel="테스트"
-							isLabel={true}
-							errorMsg="이것은 오류일 때 나타납니다"
-							inputAttr={{ type: "text", placeholder: "입력하세요" }}
-						/>
-					</section> */}
 				</main>
 			</div>
 		</div>
