f41b40e0360e0dc093dd819cea883324a33e9dbc
shinnh2 <nahyeon.shin9@gmail.com>
2023-09-20 18:27:00 +0900

    [feat/fe-validateEmail] 이메일 인증 페이지 라우터 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import ValidateEmail from "./pages/ValidateEmail";
 
 function App() {
 	return (
@@ -17,6 +18,7 @@ function App() {
 						<Routes>
 							<Route path="/login" element={<Login />} />
 							<Route path="/signup" element={<Signup />} />
+							<Route path="/validateEmail" element={<ValidateEmail />} />
 						</Routes>
 					</main>
 				</div>
