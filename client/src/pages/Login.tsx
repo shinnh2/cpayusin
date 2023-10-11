5036f1e6a239440ec6b699ff4a5957bff13c2867
shinnh2 <nahyeon.shin9@gmail.com>
2023-10-12 04:38:12 +0900

    [feat/fe-validate] 유효성 검사 기능 추가

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,48 @@
 import Input from "./../components/Input";
 import Button from "./../components/Button";
+import { validator, ValidatorStatus } from "../assets/validater";
+import { useState } from "react";
 
 const Login = () => {
+	const [value, setValue] = useState({
+		email: "",
+		password: "",
+	});
+	const [isError, setIsError] = useState({
+		email: false,
+		password: false,
+	});
+	const setEmailValue = (value: string) => {
+		setValue((prevState) => ({
+			...prevState,
+			email: value,
+		}));
+	};
+	const setPasswordValue = (value: string) => {
+		setValue((prevState) => ({
+			...prevState,
+			password: value,
+		}));
+	};
+	//유효성 검사
+	const validatorStatusEmail: ValidatorStatus = {
+		value: value.email,
+		isRequired: true,
+		valueType: "email",
+	};
+	const validatorStatusPassword: ValidatorStatus = {
+		value: value.password,
+		isRequired: true,
+		valueType: "password",
+	};
+	const handleOnclick = () => {
+		setIsError((prevState) => ({
+			...prevState,
+			email: !validator(validatorStatusEmail),
+			password: !validator(validatorStatusPassword),
+		}));
+	};
+
 	return (
 		<div className="input_box login_box col_4">
 			<h3 className="title_h3">로그인</h3>
@@ -11,14 +52,25 @@ const Login = () => {
 					isLabel={true}
 					errorMsg="올바른 이메일을 입력해 주세요."
 					inputAttr={{ type: "text", placeholder: "이메일을 입력하세요" }}
+					setInputValue={setEmailValue}
+					inputValue={value.email}
+					isError={isError.email}
 				/>
 				<Input
 					InputLabel="비밀번호"
 					isLabel={true}
 					errorMsg="올바른 비밀번호를 입력해 주세요."
 					inputAttr={{ type: "password", placeholder: "비밀번호를 입력하세요" }}
+					setInputValue={setPasswordValue}
+					inputValue={value.password}
+					isError={isError.password}
 				/>
-				<Button buttonType="primary" buttonSize="big" buttonLabel="로그인" />
+				<Button
+					buttonType="primary"
+					buttonSize="big"
+					buttonLabel="로그인"
+					onClick={handleOnclick}
+				/>
 			</div>
 		</div>
 	);
