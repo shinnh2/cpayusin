4c5d4b887f4ef786b14ffa799bb28fd8a99ecc68
shinnh2 <nahyeon.shin9@gmail.com>
2023-12-28 23:13:26 +0900

    [feat/fe-writeBoardAPI] 홈화면 수정

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,16 +1,9 @@
 import SelectBox from "../components/SelectBox";
 
 const Home = () => {
-	const selectItem1 = ["값1", "값2", "값3"];
 	return (
 		<div>
-			<SelectBox
-				id="test1"
-				selectLabel="테스트"
-				isLabel={true}
-				selectItem={selectItem1}
-				placeHolder="선택하세요"
-			/>
+			<h3>HOME</h3>
 		</div>
 	);
 };
