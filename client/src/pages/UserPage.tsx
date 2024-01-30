2417b636a452bc9862b8a680e49149a38e228d55
shinnh2 <nahyeon.shin9@gmail.com>
2024-01-31 03:46:03 +0900

    [feat/fe-boardDetailAPI] Boarditem 컴포넌트 props 변경 반영으로 DummyData 삭제

--- client/src/pages/UserPage.tsx
+++ client/src/pages/UserPage.tsx
@@ -3,26 +3,26 @@ import BoardItem from "../components/Boarditem";
 import CommentItem from "../components/CommentItem";
 import { useState } from "react";
 
-const dummyData = {
-	id: 53,
-	title: "JPA란",
-	content: "~~~~~~",
-	files: [
-		{
-			originalFileName: "orca.jfif",
-			storedFileName: "ddfd5ebb-c490-4b9c-bdc0-75c2ccc9e437.jfif",
-			url: "https://jbaccount.s3.ap-northeast-2.amazonaws.com/post/ddfd5ebb-c490-4b9c-bdc0-75c2ccc9e437.jfif",
-		},
-	],
-	voteCount: 0,
-	voteStatus: false,
-	member: {
-		id: 1,
-		nickname: "운영자",
-	},
-	createdAt: "2023-09-07T18:48:14.6994829",
-	modifiedAt: "2023-09-07T18:48:14.6994829",
-};
+// const dummyData = {
+// 	id: 53,
+// 	title: "JPA란",
+// 	content: "~~~~~~",
+// 	files: [
+// 		{
+// 			originalFileName: "orca.jfif",
+// 			storedFileName: "ddfd5ebb-c490-4b9c-bdc0-75c2ccc9e437.jfif",
+// 			url: "https://jbaccount.s3.ap-northeast-2.amazonaws.com/post/ddfd5ebb-c490-4b9c-bdc0-75c2ccc9e437.jfif",
+// 		},
+// 	],
+// 	voteCount: 0,
+// 	voteStatus: false,
+// 	member: {
+// 		id: 1,
+// 		nickname: "운영자",
+// 	},
+// 	createdAt: "2023-09-07T18:48:14.6994829",
+// 	modifiedAt: "2023-09-07T18:48:14.6994829",
+// };
 
 const UserPage = () => {
 	const [tabIndex, setTabIndex] = useState(0);
@@ -72,12 +72,8 @@ const UserPage = () => {
 					{/* 작성글인 경우 */}
 					{tabIndex === 0 ? (
 						<ul className="board">
-							<li>
-								<BoardItem data={dummyData} />
-							</li>
-							<li>
-								<BoardItem data={dummyData} />
-							</li>
+							<li>{/* <BoardItem data={dummyData} /> */}</li>
+							<li>{/* <BoardItem data={dummyData} /> */}</li>
 						</ul>
 					) : null}
 					{/* 댓글인 경우 */}
