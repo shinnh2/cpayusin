26d3b4326e42179038c00ec40a5ca6a9144319fe
shinnh2 <nahyeon.shin9@gmail.com>
2024-01-31 03:51:11 +0900

    [feat/fe-boardDetailAPI] props 데이터 인터페이스 수정, 반영

--- client/src/components/Boarditem.tsx
+++ client/src/components/Boarditem.tsx
@@ -1,40 +1,67 @@
-interface Data {
-	id?: number;
+//api문서 기준
+// interface Data {
+// 	postId: number;
+// 	boardId: number;
+// 	boardName: string;
+// 	categoryId?: number;
+// 	categoryName?: string;
+// 	memberId: number;
+// 	memberName: string;
+// 	title: string;
+// 	content: string;
+// 	commentsCount?: number;
+// 	createdAt: string;
+// 	timeInfo: string;
+// }
+
+//서버 응답 기준
+interface BoardItemData {
+	postId: number;
+	boardId: number;
+	boardName: string;
+	categoryId?: number;
+	categoryName?: string;
+	memberId: number;
+	memberName: string;
+	commentsCount: number;
 	title: string;
 	content: string;
-	files?: object;
-	voteCount: number;
-	voteStatus?: boolean;
-	member: Member;
 	createdAt: string;
-	modifiedAt?: string;
-}
-interface Member {
-	id?: number;
-	nickname: string;
+	timeInfo: string;
 }
 
-const BoardItem = ({ data }: { data: Data }) => {
+const BoardItem = ({ data }: { data: BoardItemData }) => {
 	return (
 		<div className="board_item">
-			<div className="board_item_element_wrap">
-				<p className="category_tag">카테고리명</p>
-				<div className="board_info">
-					<p className="info_item comments_info">
-						<span className="icon">댓글수</span>
-						<span className="info">12</span>
-					</p>
-					<p className="info_item votes_info">
+			<a
+				href={`/board/${data.boardId}-${data.boardName}/${data.postId}`}
+				title={data.title}
+			>
+				<div className="board_item_element_wrap">
+					{data.categoryId ? (
+						<p className="category_tag">{data.categoryName}</p>
+					) : (
+						<p className="category_tag no_category">카테고리없음</p>
+					)}
+					<div className="board_info">
+						{data.commentsCount ? (
+							<p className="info_item comments_info">
+								<span className="icon">댓글수</span>
+								<span className="info">{data.commentsCount}</span>
+							</p>
+						) : null}
+						{/* <p className="info_item votes_info">
 						<span className="icon">득표수</span>
 						<span className="info">{data.voteCount}</span>
-					</p>
+					</p> */}
+					</div>
 				</div>
-			</div>
-			<h3 className="title">{data.title}</h3>
-			<div className="board_item_element_wrap">
-				<p className="board_date">2023.9.30</p>
-				<p className="board_writer">{data.member.nickname}</p>
-			</div>
+				<h3 className="title">{data.title}</h3>
+				<div className="board_item_element_wrap">
+					<p className="board_date">{data.timeInfo}</p>
+					<p className="board_writer">{data.memberName}</p>
+				</div>
+			</a>
 		</div>
 	);
 };
