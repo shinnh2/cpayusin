fb63aaacc142c7ef21cb5a964e2eefb56a3eb94d
ge0nmo <ands0927@naver.com>
2024-05-09 13:54:39 +0900

    [Test] Admin Controller Rest Docs

--- server/src/main/java/com/jbaacount/payload/response/board/BoardCreateResponse.java
+++ server/src/main/java/com/jbaacount/payload/response/board/BoardCreateResponse.java
@@ -1,24 +1,20 @@
 package com.jbaacount.payload.response.board;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Builder
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-public class BoardChildrenResponse
+@AllArgsConstructor
+public class BoardCreateResponse
 {
     private Long id;
 
     private String name;
 
-    private String type;
-
     private Integer orderIndex;
-    private Boolean isAdminOnly;
 
     private Long parentId;
+    private Boolean isAdminOnly;
 }
