badb7c058065be29347a67c82b92a88702770972
ge0nmo <ands0927@naver.com>
2024-05-23 17:49:08 +0900

    [Refactor] redirect to frontend

--- server/src/main/java/com/jbaacount/global/oauth2/OAuth2Controller.java
+++ server/src/main/java/com/jbaacount/global/oauth2/OAuth2Controller.java
@@ -1,12 +1,13 @@
 package com.jbaacount.global.oauth2;
 
-import com.jbaacount.payload.response.GlobalResponse;
-import com.jbaacount.service.AuthenticationService;
 import com.jbaacount.service.OAuth2Service;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @RequestMapping("/login/oauth2")
@@ -15,12 +16,17 @@ public class OAuth2Controller
 {
     private final OAuth2Service oAuth2Service;
 
-    @GetMapping("/code/{registrationId}")
-    public ResponseEntity getGoogleUser(@RequestParam("code") String code,
-                                        @PathVariable("registrationId") String registrationId)
-    {
-        OAuth2Response response = oAuth2Service.oauth2Login(code, registrationId);
+    @Value("${frontend-uri}")
+    private String REDIRECT_URI;
 
-        return new ResponseEntity(new GlobalResponse<>(response), HttpStatus.OK);
+    @GetMapping("/code/{registrationId}")
+    public void getGoogleUser(@RequestParam("code") String code,
+                                        @PathVariable("registrationId") String registrationId,
+                                        HttpServletResponse response) throws IOException
+    {
+        OAuth2Response data = oAuth2Service.oauth2Login(code, registrationId);
+        response.sendRedirect(UriComponentsBuilder.fromUriString(REDIRECT_URI).toUriString());
+
+        //return new ResponseEntity(new GlobalResponse<>(data), HttpStatus.OK);
     }
 }
