cf3f575c2646a2f200b2cbe553c803e590441835
shinnh2 <35826626+shinnh2@users.noreply.github.com>
2024-07-18 21:19:02 +0900

    [feat/myProfileBoard] 내가 쓴 댓글 목록  (#100)
    * [feat/myProfileBoard] 마이페이지 프로필 영역 디자인 수정
    
    * [feat/myProfileBoard] 마이페이지 내가 쓴 댓글 부분 컴포넌트 추가
    
    * [feat/myProfileBoard] 댓글 영역 컴포넌트 생성
    
    * [feat/myProfileBoard] 내가 쓴 댓글 목록 개별 아이템 api 연결

---
+++ client/src/components/CommentListPagination.tsx
@@ -0,0 +1,131 @@
+import { useEffect, useState, Dispatch } from "react";
+import CommentItem from "./CommentItem";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { getAccessToken } from "../assets/tokenActions";
+
+const CommentListPagination = () => {
+	const [pageInfo, setPageInfo] = useState({
+		totalPages: 1,
+		maxPaginationValue: 5,
+	});
+	const [nowPage, setNowPage] = useState(1);
+	const [pagenation, setPagenation] = useState(
+		generatePageRange(1, pageInfo.totalPages)
+	);
+	// const params = useParams();
+	const api = process.env.REACT_APP_API_URL;
+	const [data, setData] = useState<any[]>([]);
+	useEffect(() => {
+		const token = getAccessToken();
+		axios
+			.get(`${api}/api/v1/profile/my-comments?page=1&size=8`, {
+				headers: { Authorization: token },
+			}) //url
+			.then((response) => {
+				const pageInfo = response.data.pageInfo;
+				const totalPageNum = pageInfo.totalPages;
+				const maxPaginationNum = totalPageNum >= 5 ? 5 : totalPageNum;
+				setPageInfo({
+					totalPages: totalPageNum,
+					maxPaginationValue: maxPaginationNum,
+				});
+				setPagenation(generatePageRange(1, totalPageNum));
+				setData(response.data.data);
+			})
+			.catch((error) => {
+				console.error("에러", error);
+			});
+	}, []);
+	useEffect(() => {
+		const token = getAccessToken();
+		axios
+			.get(`${api}/api/v1/profile/my-comments?page=${nowPage}&size=8`, {
+				headers: { Authorization: token },
+			}) //url
+			.then((response) => {
+				setData(response.data.data);
+			})
+			.catch((error) => {
+				console.error("에러", error);
+			});
+	}, [nowPage]);
+
+	//페이지네이션 범위 설정 함수
+	function generatePageRange(start: number, end: number) {
+		return Array.from({ length: end + 1 - start }, (_, i) => start + i);
+	}
+
+	const handlaClickPagination = (page: number) => {
+		if (nowPage !== page) setNowPage(page);
+	};
+	//이전 클릭
+	const handleClickPrev = () => {
+		if (nowPage === 1) return;
+		if (nowPage % pageInfo.maxPaginationValue === 1)
+			setPagenation(
+				generatePageRange(nowPage - pageInfo.maxPaginationValue, nowPage - 1)
+			);
+		setNowPage(nowPage - 1);
+	};
+	//다음 클릭
+	const handleClickNext = () => {
+		if (nowPage === pageInfo.totalPages) return;
+		if (nowPage % pageInfo.maxPaginationValue === 0) {
+			if (nowPage + pageInfo.maxPaginationValue >= pageInfo.totalPages)
+				setPagenation(generatePageRange(nowPage + 1, pageInfo.totalPages));
+			else
+				setPagenation(
+					generatePageRange(nowPage + 1, nowPage + pageInfo.maxPaginationValue)
+				);
+		}
+		setNowPage(nowPage + 1);
+	};
+
+	return (
+		<div>
+			{data.length === 0 ? (
+				<div className="no_board_list">등록된 게시글이 없습니다.</div>
+			) : (
+				<ul className="comment">
+					{data.map((el, idx) => {
+						return (
+							<li key={idx}>
+								<CommentItem data={el} />
+							</li>
+						);
+					})}
+				</ul>
+			)}
+
+			{data.length === 0 ? (
+				""
+			) : (
+				<div className="pagination">
+					<button className="arrow left" onClick={handleClickPrev}>
+						이전
+					</button>
+					<ul className="page_num">
+						{pagenation.map((el, idx) => {
+							return (
+								<li
+									className={
+										el === nowPage ? "page_button active" : "page_button"
+									}
+									key={idx}
+									onClick={() => handlaClickPagination(el)}
+								>
+									{el}
+								</li>
+							);
+						})}
+					</ul>
+					<button className="arrow right" onClick={handleClickNext}>
+						다음
+					</button>
+				</div>
+			)}
+		</div>
+	);
+};
+export default CommentListPagination;
