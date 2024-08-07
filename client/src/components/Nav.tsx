7e362840a13e0b113ad3b84734858ed86260b2cb
shinnh2 <nahyeon.shin9@gmail.com>
2024-08-08 07:04:20 +0900

    [fix/240804] 메뉴 클릭시 이벤트 버블링, 캡쳐링 현상 수정/메뉴만 클릭시 모바일 네비게이션 드로어 닫히도록 수정

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -5,38 +5,57 @@ import { MouseEvent, useEffect, useState, Dispatch } from "react";
 import { NavLink } from "react-router-dom";
 import NavInfo from "./NavInfo";
 
-const createMenuNode = (data: any[]) => {
-	return data!.map((el: any, idx: number) => (
-		<li key={idx} className="nav_menu_item">
-			{el.name ? (
-				<NavLink
-					to={`/board/${el.name}`}
-					onClick={clickHandler}
-					className={({ isActive }) => (isActive ? "active" : "")}
-				>
-					{el.name}
-					{el.category && el.category.length !== 0 ? (
-						<i className="toggle_updown">
-							<img src={iconArrowDown} alt="메뉴 펼치기 접기 토글 아이콘" />
-						</i>
-					) : null}
-				</NavLink>
-			) : (
-				<a href="">{el.categoryName}</a>
-			)}
-			{el.category && el.category.length !== 0 ? (
-				<ul className="nav_menu depth2">{createMenuNode(el.category)}</ul>
-			) : null}
-		</li>
-	));
-};
-
-const clickHandler = (event: React.MouseEvent<HTMLAnchorElement>): void => {
-	event.currentTarget.classList.toggle("unfold");
-};
-
-const Nav = ({ menuData }: { menuData: any[] }) => {
+const Nav = ({
+	menuData,
+	setIsNavDrawerOn,
+}: {
+	menuData: any[];
+	setIsNavDrawerOn: React.Dispatch<React.SetStateAction<boolean>> | undefined;
+}) => {
 	const api = process.env.REACT_APP_API_URL;
+	const [unfold, setUnfold] = useState(false);
+	const createMenuNode = (data: any[]) => {
+		return data!.map((el: any, idx: number) => (
+			<li key={idx} className="nav_menu_item">
+				{el.name ? (
+					<NavLink
+						to={`/board/${el.name}`}
+						onClick={clickMenuHandler}
+						className={({ isActive }) => (isActive ? "active" : "")}
+					>
+						{el.name}
+						{el.category && el.category.length !== 0 ? (
+							<button className="toggle_updown" onClick={clickToggleHandler}>
+								<img src={iconArrowDown} alt="메뉴 펼치기 접기 토글 아이콘" />
+							</button>
+						) : null}
+					</NavLink>
+				) : (
+					<a href="">{el.categoryName}</a>
+				)}
+				{el.category && el.category.length !== 0 ? (
+					<ul className={unfold ? "nav_menu depth2 unfold" : "nav_menu depth2"}>
+						{createMenuNode(el.category)}
+					</ul>
+				) : null}
+			</li>
+		));
+	};
+
+	const clickMenuHandler = (
+		event: React.MouseEvent<HTMLAnchorElement>
+	): void => {
+		if (setIsNavDrawerOn !== undefined) setIsNavDrawerOn(false);
+	};
+	const clickToggleHandler = (
+		event: React.MouseEvent<HTMLButtonElement>
+	): void => {
+		event.currentTarget.classList.toggle("unfold");
+		event.preventDefault();
+		event.stopPropagation();
+		setUnfold((prev) => !prev);
+	};
+
 	return (
 		<nav className="nav">
 			<div className="home_link">
