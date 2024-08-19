ad4dde53b676a664c674796d80cd1b7658f1136b
shinnh2 <nahyeon.shin9@gmail.com>
2024-08-20 04:59:52 +0900

    [fix/240818] 토큰 저장 관련 테스트 후 임시 조치

--- client/src/assets/tokenActions.ts
+++ client/src/assets/tokenActions.ts
@@ -7,11 +7,14 @@ export const getAccessToken = () => {
 		const cookie_list = document.cookie.split(";");
 		const cookieAccessToken = cookie_list[0].split("token=")[1];
 		saveAccessToken(`Bearer ${cookieAccessToken}`);
+		// saveAccessToken(`${cookieAccessToken}`); //test: Bearer 빼보기
 	}
 	return localStorage.getItem("accessToken");
 };
 export const saveAccessToken = (token: string) => {
+	// const accessToken = token.split(" ")[1]; //test: Bearer 빼보기
 	localStorage.setItem("accessToken", token);
+	// localStorage.setItem("accessToken", accessToken); //test: Bearer 빼보기
 };
 export const removeAccessToken = () => {
 	if (document.cookie !== "") {
