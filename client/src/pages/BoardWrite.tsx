2f9b84421a7d0701f10780d7d71e2023d66777da
shinnh2 <nahyeon.shin9@gmail.com>
2024-08-31 18:22:11 +0900

    [fix/240831] 게시글 생성시 수정된 API 반영

--- client/src/pages/BoardWrite.tsx
+++ client/src/pages/BoardWrite.tsx
@@ -161,99 +161,26 @@ const BoardWrite = () => {
 		//데이터 전송시 설정값, 토큰
 		const postAxiosConfig = {
 			headers: {
-				"Content-Type": "multipart/form-data", // FormData를 사용할 때 Content-Type을 변경
+				// "Content-Type": "multipart/form-data", // FormData를 사용할 때 Content-Type을 변경
 				Authorization: `${getAccessToken()}`,
 			},
 		};
 
-		//이미지 없는 경우
-		if (filesFormData.get("files") === null) {
-			const postData = {
-				title: titleValue,
-				content: editorData,
-				boardId: id,
-			};
-			const newFormData: FormData = new FormData();
-			newFormData.append(
-				"data",
-				new Blob([JSON.stringify(postData)], {
-					type: "application/json",
-				})
-			);
-			axios
-				.post(`${api}/api/v1/post/create`, newFormData, postAxiosConfig) //240828임시URL변경
-				.then((response) => {
-					const newPostId = response.data.data.id;
-					navigate(`/${newPostId}`);
-				})
-				.catch((error) => {
-					alert("게시판 등록을 실패했습니다.");
-				});
-		} else {
-			//이미지 있는 경우
-			let postData = {
-				title: "images",
-				content: "",
-				boardId: id,
-			};
-			const newFormData: FormData = new FormData();
-			filesFormData.forEach((value, key) => {
-				newFormData.append(key, value);
+		const postData = {
+			title: titleValue,
+			content: editorData,
+			boardId: id,
+		};
+
+		axios
+			.post(`${api}/api/v1/post`, postData, postAxiosConfig) //240828임시URL변경
+			.then((response) => {
+				const newPostId = response.data.data.id;
+				navigate(`/${newPostId}`);
+			})
+			.catch((error) => {
+				alert("게시판 등록을 실패했습니다.");
 			});
-			newFormData.append(
-				"data",
-				new Blob([JSON.stringify(postData)], {
-					type: "application/json",
-				})
-			);
-			//1. 이미지 먼저 전송: postId, imgsUrl값 반환
-			const ImagesReponse = await uploadImage(newFormData, postAxiosConfig);
-			const [postId, filesUrls] = ImagesReponse;
-			if (postId === "" || filesUrls === "") {
-				console.log("이미지 전송 실패");
-				return;
-			}
-			console.log(postId, filesUrls);
-
-			//2. 이미지 src를 base64 -> url로 변경
-			const newContent = imgConverter(editorData, filesUrls);
-
-			//3. 이미지 최종 전송(이미지 등록시 이미 게시글 생성되었으므로 게시글 수정으로 제출)
-			postData.title = titleValue;
-			postData.content = newContent;
-			newFormData.set(
-				"data",
-				new Blob([JSON.stringify(postData)], {
-					type: "application/json",
-				})
-			);
-
-			axios
-				.patch(
-					`${api}/api/v1/post/update/${postId}`, //240828임시URL변경
-					newFormData,
-					postAxiosConfig
-				)
-				.then((_) => {
-					navigate(`/board/${nowBoardData!.name}`);
-				})
-				.catch((error) => {
-					alert("게시판 등록을 실패했습니다.");
-					if (error.response) {
-						// 서버 응답이 있을 경우 (에러 상태 코드가 반환된 경우)
-						console.error("서버 응답 에러:", error.response.data);
-						console.error("응답 상태 코드:", error.response.status);
-						console.error("응답 헤더:", error.response.headers);
-					} else if (error.request) {
-						// 요청이 전혀 되지 않았을 경우
-						console.error("요청 에러:", error.request);
-					} else {
-						// 설정에서 문제가 있어 요청이 전송되지 않은 경우
-						console.error("Axios 설정 에러:", error.message);
-					}
-					console.error("에러 구성:", error.config);
-				});
-		}
 	};
 
 	return (
