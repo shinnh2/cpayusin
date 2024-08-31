a87799b5e4906e801a383210d8c55ad2d7e9579d
shinnh2 <nahyeon.shin9@gmail.com>
2024-08-31 18:21:04 +0900

    [fix/240831] 게시글 이미지 추가시 URL 변경 새 API 적용

--- client/src/components/EditorUnit.tsx
+++ client/src/components/EditorUnit.tsx
@@ -9,6 +9,7 @@ import axios from "axios";
 type HookCallback = (url: string, text?: string) => void;
 
 const EditorUnit = React.forwardRef((props: any, ref) => {
+	const api = process.env.REACT_APP_API_URL;
 	const editorRef = useRef<Editor>(null);
 	const latestBoardDataRef = useRef(props.nowBoardData); // 최신 상태를 저장할 ref
 
@@ -22,21 +23,46 @@ const EditorUnit = React.forwardRef((props: any, ref) => {
 	}));
 
 	const handleImageUpload = (blob: Blob | any, callback: HookCallback) => {
-		//실행되는 시점: 이미지를 업로드하고 '확인'버튼을 클릭했을 때.
-		//callback이 실행되지 않으면 이미지 업로드 창이 닫히지 않는다.
-		//callback(`${blob.name}`, `${blob.name}`); //이미지 저장된 url, img 대체 텍스트
-		//callback사용용도: 이미지를 먼저 서버에 업로드하여 응답으로 오는 url이나 파일명을 html에 처리한다.
+		/*
+		실행되는 시점: 이미지를 업로드하고 '확인'버튼을 클릭했을 때, 이미지 붙여넣기 했을 때
+		-> callback이 실행되지 않으면 이미지 업로드 창이 닫히지 않는다.
+		callback(`${blob.name}`, `${blob.name}`); //이미지 저장된 url, img 대체 텍스트
+		callback사용용도: 이미지를 먼저 서버에 업로드하여 응답으로 오는 url이나 파일명을 html에 처리한다.
+		*/
 
-		//blob데이터 저장
-		props.setBlobData(blob);
+		console.log("이미지 업로드 실행");
 
-		//일단 이미지 그대로 두기
-		const reader = new FileReader();
-		reader.readAsDataURL(blob);
-		reader.onloadend = () => {
-			const base64data = reader.result;
-			callback(`${base64data}`, `${blob.name}`); //이미지 저장된 url, img 대체 텍스트
+		// //blob데이터 저장
+		// props.setBlobData(blob);
+
+		// //일단 이미지 그대로 두기
+		// const reader = new FileReader();
+		// reader.readAsDataURL(blob);
+		// reader.onloadend = () => {
+		// 	const base64data = reader.result;
+		// 	callback(`${base64data}`, `${blob.name}`); //이미지 저장된 url, img 대체 텍스트
+		// };
+
+		//이미지 API 적용
+		//이미지 대체 텍스트 처리
+		const blobName = blob.name === "" ? "사용자 이미지" : blob.name;
+		const newFormData = new FormData();
+		newFormData.append("file", blob);
+		const postAxiosConfig = {
+			headers: {
+				"Content-Type": "multipart/form-data", // FormData를 사용할 때 Content-Type을 변경
+			},
 		};
+		axios
+			.post(`${api}/api/v1/file`, newFormData, postAxiosConfig)
+			.then((response) => {
+				console.log(response.data.data.url);
+				callback(`${response.data.data.url}`, `${blobName}`); //이미지 저장된 url, img 대체 텍스트
+			})
+			.catch((error) => {
+				console.log(error);
+				alert("이미지가 정상적으로 업로드 되지 않았습니다.");
+			});
 	};
 
 	return (
